ebe0c46add21d1494052fe624ca2ffd7fb9efb46
kobaru0972 <92970448+kobaru0972@users.noreply.github.com>
2021-10-28 13:57:18 +0900

    Add files via upload

---
+++ bbs/loginchack.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_POST['user']) == true && $_POST['user'] !=''){
+    $user = $_POST['user'];
+}
+
+if(isset($_POST['user']) == true && $_POST['user'] !=''){
+    $password = $_POST['password'];
+}
+
+if ($user == 'user' && $password == 'password') {
+
+    session_start();
+    session_regenerate_id(true);
+    $_SESSION['login'] = true;
+    header('location:./ADM.php');
+}
+
+else {
+    header('location:./bbs.phpto');
+   
+}
+
