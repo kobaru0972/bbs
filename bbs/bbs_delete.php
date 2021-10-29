55921865e854fba77a5a12a2592892a622ae9e92
kobaru <yuudai.ehonnomori@gmail.com>
2021-10-29 11:41:49 +0900

    delete.phpの追加

---
+++ bbs/bbs_delete.php
@@ -0,0 +1,27 @@
+<?php
+$password = $_POST['password'];
+try{
+if ($password === "password") {
+    
+        $delete_ID =$_POST['delete_ID'];
+
+        require_once('./DBInfo.php');
+        $pdo = new PDO(DBInfo::DSN, DBInfo::USER, DBInfo::PASSWORD);
+
+        $sql = 'DELETE FROM bbs WHERE id = ?';
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(1, $delete_ID);
+        $pdo->beginTransaction();
+        $statement->execute();
+        $pdo->commit();
+        $pdo = null;
+        header('location:./ADM.php');
+    }
+
+}
+catch(PDOException $e){
+    if(isset($pdo) == true && $pdo->inTransaction() == true){
+    $pdo->rollBack();
+    }  
+}
+header('location:./ADM.php');
