5bda0f57ac638c0f334bfb72ef9bf58eacfa1238
kobaru <yuudai.ehonnomori@gmail.com>
2021-11-08 11:42:06 +0900

    管理パスワード　から　パスワード　に修正

--- bbs/ADM.php
+++ bbs/ADM.php
@@ -29,7 +29,7 @@
                             <input type="text" class="input_box float_right" required pattern="^[0-9]+$" title="半角数字で入力してください" name="delete_ID">
                         </div>
                         <div class="input_area">
-                            <p class="float_left">管理パスワード</p>
+                            <p class="float_left">パスワード</p>
                             <input type="password" class="input_box password_box float_right" maxlength ="8" required pattern="^([0-9A-Za-z]{4,8})+$" title="4～8文字の半角英数字で入力してください" name ="password">
                             <p><label><input type="checkbox" class="password_checkbox">パスワードを表示する</label></p>
                         </div>
